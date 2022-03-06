a48d134cfba202c71e29d9d80dbceeba99c2bc1e
ashaikh2007 <83594762+ashaikh2007@users.noreply.github.com>
2022-03-06 04:13:52 -0500

    Delete imgur.py

--- imgur.py
+++
@@ -1,7 +0,0 @@
-import pyimgur
-
-key = 'c59267d98c1002f'
-path = 'bordy.jpg'
-im = pyimgur.Imgur(key)
-uploaded_image = im.upload_image(path, title="Uploaded with PyImgur")
-print(uploaded_image.link)
